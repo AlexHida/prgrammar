d905f51e9a1955bce72a360e8022bfe1d50f29a2
AlexHida <96128820+AlexHida@users.noreply.github.com>
2023-08-12 12:32:58 -0500

    updates app

--- myapp/views.py
+++ myapp/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-from transformers import AutoTokenizer, T5ForConditionalGeneration
+import requests
 
-# Cargar el modelo y el tokenizador
-tokenizer = AutoTokenizer.from_pretrained("grammarly/coedit-large")
-model = T5ForConditionalGeneration.from_pretrained("grammarly/coedit-large")
+API_URL = "https://api-inference.huggingface.co/models/grammarly/coedit-large"
+API_TOKEN = "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"
 
-# Función para la corrección gramatical
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
 def corregir_gramatica(input_text):
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-    outputs = model.generate(input_ids, max_length=256)
-    edited_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return edited_text
+    payload = {"inputs": input_text}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
-# Vista para la página principal
 def index(request):
     resultado = None
     texto_original = None
     if request.method == 'POST':
         input_text = request.POST.get('texto', '')
         texto_original = input_text
-        edited_text = corregir_gramatica(input_text)
-        resultado = edited_text
+        output = corregir_gramatica(input_text)
+        if isinstance(output, list) and len(output) > 0:
+            resultado = output[0].get('generated_text', '')
     return render(request, 'index.html', {'resultado': resultado, 'texto_original': texto_original})
+
